23c094c8da1dd8bc046823d844b9e7f51604943c
Pisti332 <varga.istvan.p1@gmail.com>
2023-11-01 19:58:20 +0100

    add black and white service via TDD

--- src/main/java/com/pisti/harmonicrainbow/service/BlackAndWhiteService.java
+++ src/main/java/com/pisti/harmonicrainbow/service/BlackAndWhiteService.java
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
 import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class BlackAndWhiteService {
@@ -30,8 +31,53 @@ public class BlackAndWhiteService {
                 ByteArrayResource image = (ByteArrayResource) imageResponse.getBody();
                 BufferedImage bufferedImage = ImageIO.read(image.getInputStream());
                 byte[] pixels = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
-                // TODO
-                ByteArrayResource inputStream = new ByteArrayResource(pixels);
+
+                int width = bufferedImage.getWidth();
+                int height = bufferedImage.getHeight();
+
+                int imageType = bufferedImage.getType();
+                String formatName;
+                int[] bandOffsets;
+                if (imageType == BufferedImage.TYPE_INT_RGB) {
+                    bandOffsets = new int[]{0, 1, 2};
+                    formatName = "JPEG";
+                } else if (imageType == BufferedImage.TYPE_3BYTE_BGR) {
+                    bandOffsets = new int[]{2, 1, 0};
+                    formatName = "JPEG";
+                } else if (imageType == BufferedImage.TYPE_INT_ARGB) {
+                    bandOffsets = new int[]{0, 1, 2, 3};
+                    formatName = "PNG";
+                } else if (imageType == BufferedImage.TYPE_4BYTE_ABGR) {
+                    bandOffsets = new int[]{3, 2, 1, 0};
+                    formatName = "PNG";
+                } else {
+                    bandOffsets = new int[]{0, 1, 2};
+                    formatName = "JPEG";
+                }
+
+                mutateToBlackAndWhite(pixels, bufferedImage.getType());
+
+                DataBuffer dataBuffer = new DataBufferByte(pixels, pixels.length);
+
+                WritableRaster raster = WritableRaster.createInterleavedRaster(dataBuffer,
+                        width,
+                        height,
+                        width * bandOffsets.length,
+                        bandOffsets.length,
+                        bandOffsets,
+                        null);
+
+                BufferedImage newImage = new BufferedImage(width,
+                        height,
+                        bufferedImage.getType());
+
+                newImage.setData(raster);
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ImageIO.write(newImage, formatName, baos);
+                byte[] bytes = baos.toByteArray();
+                ByteArrayResource inputStream = new ByteArrayResource(bytes);
+
                 return ResponseEntity
                         .status(HttpStatus.OK)
                         .contentLength(inputStream.contentLength())
@@ -41,6 +87,33 @@ public class BlackAndWhiteService {
             }
         } else {
             return imageResponse;
+        }
+    }
+
+    private void mutateToBlackAndWhite(byte[] imageData, int imageType) {
+        if (imageType == BufferedImage.TYPE_3BYTE_BGR) {
+            for (int i = 0; i < imageData.length; i += 3) {
+                int blue = Byte.toUnsignedInt(imageData[i]);
+                int green = Byte.toUnsignedInt(imageData[i + 1]);
+                int red = Byte.toUnsignedInt(imageData[i + 2]);
+                int avg = (red + green + blue) / 3;
+                imageData[i] = (byte) avg;
+                imageData[i + 1] = (byte) avg;
+                imageData[i + 2] = (byte) avg;
+            }
+        }
+        else if (imageType == BufferedImage.TYPE_4BYTE_ABGR) {
+            for (int i = 0; i < imageData.length; i += 4) {
+                int blue = Byte.toUnsignedInt(imageData[i + 1]);
+                int green = Byte.toUnsignedInt(imageData[i + 2]);
+                int red = Byte.toUnsignedInt(imageData[i + 3]);
+                int avg = (red + green + blue) / 3;
+                imageData[i + 1] = (byte) avg;
+                imageData[i + 2] = (byte) avg;
+                imageData[i + 3] = (byte) avg;
+            }
+        } else {
+            System.out.println("Unsupported bufferedimage type!");
         }
     }
 }
--- src/test/java/com/pisti/harmonicrainbow/service/BlackAndWhiteServiceTest.java
+++ src/test/java/com/pisti/harmonicrainbow/service/BlackAndWhiteServiceTest.java
@@ -1,50 +1,84 @@
 package com.pisti.harmonicrainbow.service;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MvcResult;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BlackAndWhiteServiceTest {
-//    @Test
-//    @WithMockUser(username = "testuser")
-//    void isImageBlackAndWhite() throws Exception {
-//        String url = "/api/service/black-and-white?name=" + name + "&email=" + email;
-//        MvcResult mvcResult = mockMvc.perform(post(url))
-//                .andReturn();
-//        byte[] imageContent = mvcResult.getResponse().getContentAsByteArray();
-//        InputStream is = new ByteArrayInputStream(imageContent);
-//        BufferedImage newBi = ImageIO.read(is);
-//        byte[] pixels = ((DataBufferByte) newBi.getRaster().getDataBuffer()).getData();
-//        if (newBi.getType() == BufferedImage.TYPE_INT_ARGB ||
-//                newBi.getType() == BufferedImage.TYPE_4BYTE_ABGR) {
-//            for (int i = 0; i < pixels.length; i += 4) {
-//                byte f = pixels[i + 3];
-//                byte s = pixels[i + 2];
-//                byte t = pixels[i + 1];
-//                if (f != s || f != t) {
-//                    fail();
-//                }
-//            }
-//        } else if (newBi.getType() == BufferedImage.TYPE_3BYTE_BGR ||
-//                newBi.getType() == BufferedImage.TYPE_INT_RGB) {
-//            for (int i = 0; i < pixels.length; i += 3) {
-//                byte f = pixels[i];
-//                byte s = pixels[i + 1];
-//                byte t = pixels[i + 2];
-//                if (f != s || f != t) {
-//                    fail();
-//                }
-//            }
-//        }
-//
-//    }
+    @Autowired
+    private BlackAndWhiteService blackAndWhiteService;
+    private BufferedImage bufferedImage;
+
+    @MockBean
+    private ImageService imageService;
+
+    @BeforeAll
+    void readImage() throws IOException {
+        String image = "src/test/java/com/pisti/harmonicrainbow/service/images/test.jpg";
+        this.bufferedImage = ImageIO.read(new File(image));
+    }
+
+    @BeforeEach
+    void setup() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(this.bufferedImage, "jpg", baos);
+        byte[] bytes = baos.toByteArray();
+        ByteArrayResource inputStream = new ByteArrayResource(bytes);
+        ResponseEntity<Object> response = ResponseEntity
+                .status(HttpStatus.OK)
+                .contentLength(inputStream.contentLength())
+                .body(inputStream);
+        when(imageService.getImageByEmailAndName("test@test.com", "test")).thenReturn(response);
+    }
+
+    @Test
+    @WithMockUser(username = "testuser")
+    void isImageBlackAndWhite() throws IOException {
+        ResponseEntity<Object> response = blackAndWhiteService.getBlackAndWhite("test@test.com", "test");
+        ByteArrayResource image = (ByteArrayResource) response.getBody();
+        BufferedImage bufferedImage = ImageIO.read(image.getInputStream());
+        byte[] colorValues = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
+        if (bufferedImage.getType() == BufferedImage.TYPE_INT_ARGB ||
+                bufferedImage.getType() == BufferedImage.TYPE_4BYTE_ABGR) {
+            for (int i = 0; i < colorValues.length; i += 4) {
+                byte f = colorValues[i + 3];
+                byte s = colorValues[i + 2];
+                byte t = colorValues[i + 1];
+                if (f != s || f != t) {
+                    fail();
+                }
+            }
+        } else if (bufferedImage.getType() == BufferedImage.TYPE_3BYTE_BGR ||
+                bufferedImage.getType() == BufferedImage.TYPE_INT_RGB) {
+            for (int i = 0; i < colorValues.length; i += 3) {
+                byte f = colorValues[i];
+                byte s = colorValues[i + 1];
+                byte t = colorValues[i + 2];
+                if (f != s || f != t) {
+                    fail();
+                }
+            }
+        }
+
+    }
 }
