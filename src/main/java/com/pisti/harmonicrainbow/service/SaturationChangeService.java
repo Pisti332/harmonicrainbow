8caf72ac76a04a45910aca94f9d0ae9356ce6867
pisti332 <varga.istvan.p1@gmail.com>
2024-01-26 19:05:02 +0100

    refactor saturation change service to avoid code duplications

--- src/main/java/com/pisti/harmonicrainbow/service/SaturationChangeService.java
+++ src/main/java/com/pisti/harmonicrainbow/service/SaturationChangeService.java
@@ -65,74 +65,46 @@ public class SaturationChangeService {
 
     private void mutateSaturation(byte[] colorValues, int imageType, int saturation) {
         if (imageType == BufferedImage.TYPE_3BYTE_BGR) {
-            if (saturation < 0 && saturation >= -100) {
-                for (int i = 0; i < colorValues.length; i += 3) {
-                    int red = Byte.toUnsignedInt(colorValues[i + 2]);
-                    int green = Byte.toUnsignedInt(colorValues[i + 1]);
-                    int blue = Byte.toUnsignedInt(colorValues[i]);
+            for (int i = 0; i < colorValues.length; i += 3) {
+                int red = Byte.toUnsignedInt(colorValues[i + 2]);
+                int green = Byte.toUnsignedInt(colorValues[i + 1]);
+                int blue = Byte.toUnsignedInt(colorValues[i]);
 
-                    Map<String, Float> hsl = hslAndRgbConverter.convertRGBtoHSL(red, green, blue);
-                    float newSaturation = hsl.get("s") - hsl.get("s") / 100 * saturation * -1;
-                    Map<String, Integer> rgb = hslAndRgbConverter.convertHSLtoRGB(hsl.get("h"),
-                            newSaturation,
-                            hsl.get("l"));
-
-                    colorValues[i + 2] = rgb.get("r").byteValue();
-                    colorValues[i + 1] = rgb.get("g").byteValue();
-                    colorValues[i] = rgb.get("b").byteValue();
+                Map<String, Float> hsl = hslAndRgbConverter.convertRGBtoHSL(red, green, blue);
+                float newSaturation = hsl.get("s");
+                if (saturation < 0 && saturation >= -100) {
+                    newSaturation = hsl.get("s") - hsl.get("s") / 100 * saturation * -1;
+                } else if (saturation > 0 && saturation <= 100) {
+                    newSaturation = hsl.get("s") + (100 - hsl.get("s")) / 100 * saturation;
                 }
-            } else if (saturation > 0 && saturation <= 100) {
-                for (int i = 0; i < colorValues.length; i += 3) {
-                    int red = Byte.toUnsignedInt(colorValues[i + 2]);
-                    int green = Byte.toUnsignedInt(colorValues[i + 1]);
-                    int blue = Byte.toUnsignedInt(colorValues[i]);
+                Map<String, Integer> rgb = hslAndRgbConverter.convertHSLtoRGB(hsl.get("h"),
+                        newSaturation,
+                        hsl.get("l"));
 
-                    Map<String, Float> hsl = hslAndRgbConverter.convertRGBtoHSL(red, green, blue);
-                    float newSaturation = hsl.get("s") + (100 - hsl.get("s")) / 100 * saturation;
-                    Map<String, Integer> rgb = hslAndRgbConverter.convertHSLtoRGB(hsl.get("h"),
-                            newSaturation,
-                            hsl.get("l"));
-
-
-                    colorValues[i + 2] = rgb.get("r").byteValue();
-                    colorValues[i + 1] = rgb.get("g").byteValue();
-                    colorValues[i] = rgb.get("b").byteValue();
-                }
+                colorValues[i + 2] = rgb.get("r").byteValue();
+                colorValues[i + 1] = rgb.get("g").byteValue();
+                colorValues[i] = rgb.get("b").byteValue();
             }
         } else if (imageType == BufferedImage.TYPE_4BYTE_ABGR) {
-            if (saturation < 0 && saturation >= -100) {
-                for (int i = 0; i < colorValues.length; i += 4) {
-                    int red = Byte.toUnsignedInt(colorValues[i + 3]);
-                    int green = Byte.toUnsignedInt(colorValues[i + 2]);
-                    int blue = Byte.toUnsignedInt(colorValues[i + 1]);
+            for (int i = 0; i < colorValues.length; i += 4) {
+                int red = Byte.toUnsignedInt(colorValues[i + 3]);
+                int green = Byte.toUnsignedInt(colorValues[i + 2]);
+                int blue = Byte.toUnsignedInt(colorValues[i + 1]);
 
-                    Map<String, Float> hsl = hslAndRgbConverter.convertRGBtoHSL(red, green, blue);
-                    float newSaturation = hsl.get("s") - hsl.get("s") / 100 * saturation * -1;
-                    Map<String, Integer> rgb = hslAndRgbConverter.convertHSLtoRGB(hsl.get("h"),
-                            newSaturation,
-                            hsl.get("l"));
-
-                    colorValues[i + 3] = rgb.get("r").byteValue();
-                    colorValues[i + 2] = rgb.get("g").byteValue();
-                    colorValues[i + 1] = rgb.get("b").byteValue();
+                Map<String, Float> hsl = hslAndRgbConverter.convertRGBtoHSL(red, green, blue);
+                float newSaturation = hsl.get("s");
+                if (saturation < 0 && saturation >= -100) {
+                    newSaturation = hsl.get("s") - hsl.get("s") / 100 * saturation * -1;
+                } else if (saturation > 0 && saturation <= 100) {
+                    newSaturation = hsl.get("s") + (100 - hsl.get("s")) / 100 * saturation * -1;
                 }
+                Map<String, Integer> rgb = hslAndRgbConverter.convertHSLtoRGB(hsl.get("h"),
+                        newSaturation,
+                        hsl.get("l"));
 
-            } else if (saturation > 0 && saturation <= 100) {
-                for (int i = 0; i < colorValues.length; i += 4) {
-                    int red = Byte.toUnsignedInt(colorValues[i + 3]);
-                    int green = Byte.toUnsignedInt(colorValues[i + 2]);
-                    int blue = Byte.toUnsignedInt(colorValues[i + 1]);
-
-                    Map<String, Float> hsl = hslAndRgbConverter.convertRGBtoHSL(red, green, blue);
-                    float newSaturation = hsl.get("s") + (100 - hsl.get("s")) / 100 * saturation * -1;
-                    Map<String, Integer> rgb = hslAndRgbConverter.convertHSLtoRGB(hsl.get("h"),
-                            newSaturation,
-                            hsl.get("l"));
-
-                    colorValues[i + 3] = rgb.get("r").byteValue();
-                    colorValues[i + 2] = rgb.get("g").byteValue();
-                    colorValues[i + 1] = rgb.get("b").byteValue();
-                }
+                colorValues[i + 3] = rgb.get("r").byteValue();
+                colorValues[i + 2] = rgb.get("g").byteValue();
+                colorValues[i + 1] = rgb.get("b").byteValue();
             }
         } else {
             System.out.println("Unsupported bufferedimage type!");
