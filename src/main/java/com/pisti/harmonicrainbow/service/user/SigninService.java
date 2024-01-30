0d2748d59d4a1bba98e09c69972abe1a8952f877
Pisti332 <varga.istvan.p1@gmail.com>
2024-01-30 17:21:20 +0100

    decouple login and register services from controllers and handle messages on frontend

--- src/main/java/com/pisti/harmonicrainbow/service/user/SigninService.java
+++ src/main/java/com/pisti/harmonicrainbow/service/user/SigninService.java
@@ -42,21 +42,18 @@ public class SigninService {
         this.myUserDetailsService = myUserDetailsService;
     }
 
-    public ResponseEntity<Object> signinUser(SignupForm signupForm) {
+    public Map<String, String> signinUser(SignupForm signupForm) {
         Map<String, String> response = new HashMap<>();
         response.put("isLoginSuccessful", "false");
         if (!Validator.validateEmail(signupForm.email())) {
             response.put("reason", "wrong email format");
-            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+            return response;
         }
 
         response.put("reason", "no user with this email and password combination");
-        System.out.println(signupForm.email());
-        System.out.println(passwordEncoder.encode(signupForm.password()));
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(signupForm.email(), signupForm.password())
         );
-
         UserDetails user = myUserDetailsService.loadUserByUsername(authentication.getName());
 
         UserDetails userDetails = org.springframework.security.core.userdetails.User
@@ -67,12 +64,11 @@ public class SigninService {
         String token = jwtService.generateToken(userDetails);
         if (!myUserDetailsService.isUserActive(authentication.getName())) {
             response.put("reason", "email hasn't been confirmed yet");
-            return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+            return response;
         }
         response.put("isLoginSuccessful", "true");
         response.put("reason", "valid credentials");
-        MultiValueMap<String, String> headers = new HttpHeaders();
-        headers.put("Authorization", Collections.singletonList("Bearer " + token));
-        return new ResponseEntity<>(response, headers, HttpStatus.OK);
+        response.put("auth", token);
+        return response;
     }
 }
